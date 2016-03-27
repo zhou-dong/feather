0120f92aa4bd0956b9d5f866e71bac929544e5c2
zhou-dong <82224165@qq.com>
2016-03-27 01:17:22 -0500

    config maven module

---
+++ common/src/main/java/org/common/util/FileUtil.java
@@ -0,0 +1,23 @@
+package org.common.util;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileUtil {
+
+	static Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+	public static void close(Closeable closeable) {
+		if (closeable != null) {
+			try {
+				closeable.close();
+			} catch (IOException e) {
+				logger.error("close IO error", e);
+			}
+		}
+	}
+
+}
