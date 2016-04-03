c9f2ea531cea6a26e93a428d33b3e317ff7d844d
zhou-dong <82224165@qq.com>
2016-04-03 01:45:59 -0500

    update

---
+++ app/src/test/java/org/feather/app/TestJetty.java
@@ -0,0 +1,5 @@
+package org.feather.app;
+
+public class TestJetty {
+
+}
---
+++ app/src/test/java/org/feather/app/TestJsonRpc.java
@@ -0,0 +1,13 @@
+package org.feather.app;
+
+import org.junit.Test;
+
+public class TestJsonRpc {
+
+	@Test
+	public void test1() {
+		String url = "http://software.dzhuvinov.com/json-rpc-2.0-server.html";
+		System.out.println(url);
+	}
+
+}
